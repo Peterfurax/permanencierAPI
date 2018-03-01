0d35c7945a0fe832b9c7e1cf074b5ef0b8788187
MONTOYA Pierre <pmontoya@lesechos.fr>
2018-03-01 18:11:03 +0100

    jeudi

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,24 @@
 const liste = require("../liste_journalistes")
+
+
+
+let Autors_Re = ""
+let mess = ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const liste = [1950, 1960, 1970, 1980, 1990, 2000, 2010];
 /**
  * Provides modules WEB server
  * @module WEB
@@ -14,26 +34,53 @@ const express = require("express");
  */
 const router = express.Router();
 
+
+// /**
+//  * Configure `router` to get `/json` authors
+//  * @property /json
+//  * @type {function}
+//  * @default {json} json
+//  */
+// router.get("/", (req, res) => {
+//   console.log(liste)
+//   res.json(liste).status(200);
+// });
+
+
+
+
 /**
  * Configure `router` to get `/json` authors
  * @property /json
  * @type {function}
  * @default {json} json
  */
-router.get("/json", (req, res) => {
-  console.log(liste)
+router.get("/listePerma", (req, res) => {
+  // console.log(liste)
+  res.json([Autors_Re,mess, liste]).status(200);
+});
+
+router.get("/save", (req, res) => {
+  // console.log(liste)
+  res.json([Autors_Re,mess]).status(200);
+});
+
+router.get("/", (req, res) => {
+  // console.log(liste)
   res.json(liste).status(200);
 });
 
-
-
 /**
  * Configure `router` to get `/json` authors
  * @property /json
  * @type {function}
  * @default {json} json
  */
-router.get("/:authors", (req, res) => {
+router.get("/perma/:authors/mess/:mess", (req, res) => {
+  Autors_Re = req.params.authors
+  mess = req.params.mess
+  console.dir(req.params.authors)
+  console.dir(req.params.mess)
   res.send("Merci les permanencier(es) vont être ==>" + req.params.authors).status(200);
 });
 
